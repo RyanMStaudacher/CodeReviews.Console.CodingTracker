c371b190f6bf87f8806c24470e37232738004c46
Ryan Staudacher <ryanmstaudacher@gmail.com>
2025-02-15 02:08:30 -0600

    Initial commit.

---
+++ CodingTracker/CodingTracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using Microsoft.Data.Sqlite;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Dapper;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace CodingTracker
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            AnsiConsole.Write(new Markup("[bold green]Hello[/] [underline blue]World![/]"));
+            Console.ReadKey();
+        }
+    }
+}
